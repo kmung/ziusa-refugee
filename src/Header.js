34ec70601bb08caccbe2de9d23a2096d702479f1
Khawm Mung <johnmungtawng5@gmail.com>
2022-11-23 18:25:03 -0600

    nav bar style

--- src/Header.js
+++ src/Header.js
@@ -1,17 +1,23 @@
 // CSS imported in App.js
 import logo from "./img/ziusa-logo.png";
+//import Home from "./pages/Home";
 
 export default function Navbar() {
     return <header>
         <nav className="nav-bar">
-            <img src={logo} alt="ZIUSA Logo"></img>
-            <a className="logo" href="#">Zomi Refugee Program</a>
-            <ul>
-                <li>Home</li>
-                <li>About Us</li>
-                <li>Team</li>
-                <li>Contact Us</li>
-            </ul>
+            <div className="brand">
+                <img id="logo" src={logo} alt="ZIUSA Logo"></img>
+                <a href="#" id="site-title">Zomi Refugee Program</a>
+            </div>
+            <div className="nav-menu">
+                <ul className="menu-items">
+                    <li><a href="#">Home</a></li>
+                    <li><a href="#">About Us</a></li>
+                    <li><a href="#">Team</a></li>
+                    <li><a href="#">Services</a></li>
+                </ul>
+                <button type="button">Contact Us</button>
+            </div>
         </nav>
     </header>
 }
