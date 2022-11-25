853caec9d4aac9b486a5a0ea39789731b5487ba0
Khawm Mung <johnmungtawng5@gmail.com>
2022-11-24 21:20:04 -0600

    begin developing homepage

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,11 @@
 // use main.css
 export default function Home() {
-    return <section className="hero">
-        <h1>Zomi Refugee Program</h1>
+    return <section className="hero-wrapper">
+        <div className="hero">
+            <div className="hero-txt">
+                <h1>Zomi Refugee Program</h1>
+            </div>
+            <div className="hero-media"></div>
+        </div>
     </section>
 }
