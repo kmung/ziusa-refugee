6ed7c7465172995b7b85e4731c2ccb2710a3fbbd
Khawm Mung <johnmungtawng5@gmail.com>
2022-11-28 00:07:01 -0600

    about page v1.3

--- src/pages/About.js
+++ src/pages/About.js
@@ -14,7 +14,7 @@ export default function About() {
             <p>The goal of the Refugee Program is to ensure that every Zomi Refugee, who have been persecuted on the basis of race, religion, nationality, political opinion or membership in a particular social group, will receive proper treatment and protection under the Refugees’ rights, and to work together to uphold humanitarian response to refugee through the U.S. Resettlement Program.</p>
             <h2>Mission Statement</h2>
             <p>Zomi Refugee Program carries out the aims and objectives of the Zomi Innkuan USA (ZIUSA) to serve and advocate for the Zomi Refugees around the world. It offers services targeting gaps within the refugee community by ensuring the continuation of services in their efforts towards self-sufficiency and integration such as social level assistance, skills development (English language education for adults,) health promotion and cultural orientation, youth mentoring, and case management.</p>
-            <h2>Vission Statement</h2>
+            <h2>Vision Statement</h2>
             <p>Zomi Refugee Program is a leading network for the enhancement of cultural and community development in the Zomi refugee community to have the opportunity to integrate into the larger American society by interacting and sharing their unique cultures and skills.</p>
             <h2 id='leadership-heading'>Leadership Team</h2>
             <div className='leadership-section'>
